8c7a7f1b0efa4d99bb807c137f40a552b1e5862c
simonloira <simonloira@gmail.com>
2026-05-05 02:46:44 +0200

    refactor(settings): replace hardcoded config with environment variables
    - replace getcwd with Path for a more reliable behavior.
    - load sensitive vars from environment variables
    - add debugging flag
    - improve type hints

--- settings.py
+++ settings.py
@@ -1,34 +1,43 @@
 #Lo importo en rwConnectLogo y en plc_controller
 from dataclasses import dataclass
-from os import getcwd
+from typing import Final, Literal
+from pathlib import Path
+from load_var_env import load_vars_env
+from os import environ
 
 # --- Configuración de la Aplicación ---
+MAIN_PATH:Final[Path] = Path(__file__).resolve().parent
+load_vars_env()
 
 @dataclass
 class Settings:
-    MAIN_PATH = getcwd()
   # 1. Configuración de rutas
     # Login
-    LOGIN_PATH = f"{MAIN_PATH}/backend/login/login.txt"
+    USER:Final[str] = environ['USER_LOGIN']
+    PASSWORD:Final[str] = environ['PASSWORD_LOGIN']
+    SECRET_KEY: Final[str] = environ['SECRET_JWT_KEY']
+    ACCESS_TOKEN_MINUTES:Final[int] = 120
     # Clima
-    MAIN_CLIMATE_PATH = f"{MAIN_PATH}/backend/clima"
-    CLIMATE_DATA_PATH = f"{MAIN_CLIMATE_PATH}/datos_clima"
-    CONFIG_APIS_FILE_PATH = f"{MAIN_CLIMATE_PATH}/datos_clima/CLIMATE_APIS_CONFIG.json"
+    MAIN_CLIMATE_PATH:Final[Path] = Path(MAIN_PATH/"backend"/"clima")
+    CLIMATE_DATA_PATH:Final[str] = str(MAIN_CLIMATE_PATH/"datos_clima")
+    CONFIG_APIS_FILE_PATH:Final[str] = str(MAIN_CLIMATE_PATH/"datos_clima"/"CLIMATE_APIS_CONFIG.json")
+    METEOGAL_API:Final[str] = environ['METEOGALICIA_API_KEY']
     # PLC
-    VM_PATH = f"{MAIN_PATH}/backend/PLC/config/virtualMemories.json"
-    ZM_PATH = f"{MAIN_PATH}/backend/PLC/config/zonesMemories.json"
-    TZ_PATH = f"{MAIN_PATH}/backend/PLC/config/timeZonesData.json"
-    SSM_PATH = F"{MAIN_PATH}/backend/PLC/config/statusSystemMemories.json"
+    VM_PATH:Final[str] = str(MAIN_PATH/"backend"/"PLC"/"config"/"virtualMemories.json") 
+    ZM_PATH:Final[str] = str(MAIN_PATH/"backend"/"PLC"/"config"/"zonesMemories.json")
+    TZ_PATH:Final[str] = str(MAIN_PATH/"backend"/"PLC"/"config"/"timeZonesData.json") 
+    SSM_PATH:Final[str] = str(MAIN_PATH/"backend"/"PLC"/"config"/"statusSystemMemories.json")
 
   # 2. Configuración del PLC
-    MEMORIE_BYTES_READ = 5 #Bytes de memoria a leer
-    IP_LOGO: str = "192.168.2.252"
-    LOCAL_TSAP = 0x1000
-    REMOTE_TSAP = 0x2000
+    MEMORIE_BYTES_READ:Final[int] = 5 #Bytes de memoria a leer
+    IP_LOGO:Final[str] = "192.168.1.252" #"192.168.2.252"
+    LOCAL_TSAP:Final[Literal[4096]] = 0x1000
+    REMOTE_TSAP:Final[Literal[8192]]  = 0x2000
     
   # 3. Configuración del Servidor
-    SERVER_HOST: str = "0.0.0.0"
-    SERVER_PORT: int = 8000
+    SERVER_HOST: Final[str]  = "0.0.0.0"
+    SERVER_PORT:Final[int] = 8000
+    DEBUGGING:Final[bool] = False
     
   # 4. Constantes de Tarea ya veré en algún momento si me interesa poner 
     # un archivo de configuración de marcas.
